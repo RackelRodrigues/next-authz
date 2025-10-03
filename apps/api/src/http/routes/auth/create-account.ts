502af1bd0e35a03c960efa8ceb99c3570fa0229e
RackelRodrigues <rackelrodrigues247@gmail.com>
2025-10-03 12:53:42 -0300

    feat: create user signup route with Prisma and bcrypt

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,7 +1,9 @@
+import { prisma } from "@/http/lib/prisma";
 import { FastifyCorsOptions } from "@fastify/cors"
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
+import {hash} from 'bcryptjs'
 
 export async function CreaateAccount(app: FastifyInstance){
  app.withTypeProvider<ZodTypeProvider>().post('/users', {
@@ -11,7 +13,28 @@ export async function CreaateAccount(app: FastifyInstance){
             email: z.string().email(),
             password: z.string().min(6)})
     }
- },()=>{
-    return {message: 'Create Account'}
+ },async (request, reply)=>{
+    const {name, email, password} = request.body
+
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {email}
+    })
+
+    if(userWithSameEmail){
+      return reply.status(400).send({message: 'E-mail already registered.'})
+    }
+
+    const passwordHash = await hash(password, 6)
+    
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: passwordHash
+      }
+    })
+
+
+    return reply.status(201).send()
  })
 }
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,8 +1,2 @@
 import {defineAbilityFor} from "@saas/auth"
 
-const ability = defineAbilityFor({role: 'ADMIN'})
-
-const userCanInviteSomeoneElse = ability.can("invite", "User")
-
-
-console.log(userCanInviteSomeoneElse)
